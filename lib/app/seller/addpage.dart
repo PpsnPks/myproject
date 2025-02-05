e5c9bf32a00c07e0a4d4f8a574ba27e797b277a5
NoNnO7 <non.praisri9029@gmail.com>
2025-02-05 17:02:01 +0700

    sell

--- lib/app/seller/addpage.dart
+++ lib/app/seller/addpage.dart
@@ -118,7 +118,7 @@ class _AddProductPageState extends State<AddProductPage> {
   @override
   void initState() {
   super.initState();
-  _productTypeController.text = 'ขาย'; // หรือค่าเริ่มต้นที่เหมาะสม
+  _productTypeController.text = 'sell'; // หรือค่าเริ่มต้นที่เหมาะสม
 }
   Widget build(BuildContext context) {
   return Scaffold(
